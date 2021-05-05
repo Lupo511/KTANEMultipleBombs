b6706176742fb7190895f87c960c0c89c2ef59ae
Lupo511 <lupo511@mail.com>
2021-05-05 16:43:30 +0200

    Added PostToStart to StateManager.

---
+++ MultipleBombsAssembly/MultipleBombsAssembly/StartNotifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MultipleBombsAssembly
+{
+    public class StartNotifier : MonoBehaviour
+    {
+        public event Action OnStart;
+
+        public void Start()
+        {
+            if (OnStart != null)
+                OnStart();
+        }
+    }
+}
--- MultipleBombsAssembly/MultipleBombsAssembly/StateManager.cs
+++ MultipleBombsAssembly/MultipleBombsAssembly/StateManager.cs
@@ -7,11 +7,14 @@ namespace MultipleBombsAssembly
 {
     public class StateManager
     {
+        private StartNotifier currentStartNotifier;
+        private Queue<Action> startPostedDelegates;
         private Queue<Action> lateUpdatePostedDelegates;
         private List<StateCoroutine> coroutines;
 
         public StateManager()
         {
+            startPostedDelegates = new Queue<Action>();
             lateUpdatePostedDelegates = new Queue<Action>();
             coroutines = new List<StateCoroutine>();
         }
@@ -19,6 +22,17 @@ namespace MultipleBombsAssembly
         public virtual void Update() { }
 
         public virtual void LateUpdate() { }
+
+        private void CurrentStartNotifier_OnStart()
+        {
+            while (startPostedDelegates.Count > 0)
+            {
+                startPostedDelegates.Dequeue()();
+            }
+
+            UnityEngine.Object.Destroy(currentStartNotifier.gameObject);
+            currentStartNotifier = null;
+        }
 
         public void RunLateUpdateDelegates()
         {
@@ -36,6 +50,17 @@ namespace MultipleBombsAssembly
                 if (coroutine.CoroutineUpdate())
                     coroutines.RemoveAt(i);
             }
+        }
+
+        public void PostToStart(Action action)
+        {
+            if (currentStartNotifier == null)
+            {
+                currentStartNotifier = new UnityEngine.GameObject("StartNotifier").AddComponent<StartNotifier>();
+                currentStartNotifier.OnStart += CurrentStartNotifier_OnStart;
+            }
+
+            startPostedDelegates.Enqueue(action);
         }
 
         public void PostToLateUpdate(Action action)
