ff5ed35021f2758e47d4069f74a5e52433b46e8f
Lupo511 <lupo511@mail.com>
2023-09-05 19:26:54 +0200

    Removed "GetTickingBombs" and "GetSolvedBombs" from API.

--- MultipleBombsAssembly/MultipleBombsAssembly/MultipleBombsProperties.cs
+++ MultipleBombsAssembly/MultipleBombsAssembly/MultipleBombsProperties.cs
@@ -13,14 +13,6 @@ namespace MultipleBombsAssembly
         {
             AddProperty("CurrentMaximumBombCount", () => MultipleBombsModManager.GetMaximumBombs(), null);
             AddProperty("CurrentFreePlayBombCount", () => { return MultipleBombs.CurrentFreeplayBombCount; }, (object value) => { MultipleBombs.CurrentFreeplayBombCount = (int)value; });
-            AddProperty("GetTickingBombs", () => new Func<List<KMBomb>>(() =>
-            {
-                return SceneManager.Instance?.GameplayState?.Bombs.Where((bomb) => bomb.GetTimer()?.IsUpdating ?? false).Select((bomb) => bomb.GetComponent<KMBomb>() ?? bomb.gameObject.AddComponent<KMBomb>()).ToList() ?? new List<KMBomb>();
-            }), null);
-            AddProperty("GetSolvedBombs", () => new Func<List<KMBomb>>(() =>
-            {
-                return SceneManager.Instance?.GameplayState?.Bombs.Where((bomb) => bomb.IsSolved()).Select((bomb) => bomb.GetComponent<KMBomb>() ?? bomb.gameObject.AddComponent<KMBomb>()).ToList() ?? new List<KMBomb>();
-            }), null);
             AddProperty("CreateBombInfoForBomb", () => new Func<KMBomb, KMBombInfo>((kmBomb) =>
             {
                 Bomb bomb = kmBomb.GetComponent<Bomb>();
